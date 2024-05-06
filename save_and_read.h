b8748b188d460bbada50c0ab21b74319ef2d670c
SidimDoma <167631201+SidimDoma@users.noreply.github.com>
2024-05-06 17:02:00 +0300

    Update save_and_read.h

--- save_and_read.h
+++ save_and_read.h
@@ -1,10 +1,10 @@
 #ifndef SAVE_AND_READ_H_INCLUDED
 #define SAVE_AND_READ_H_INCLUDED
 
-// Сохранение в файл
+//Saves object to file
 void saveToFile();
 
-// Чтение из файла
+//Reads file
 void readFromFile();
 
 #endif // SAVE_AND_READ_H_INCLUDED
