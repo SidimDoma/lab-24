7d111b0c61d0f795ed92c4a1958dd7139bc54d3e
SidimDoma <167631201+SidimDoma@users.noreply.github.com>
2024-05-06 16:41:12 +0300

    add_and_delete.c

--- add_and_delete.c
+++ add_and_delete.c
@@ -2,31 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include "base.h"
-// Функция для добавления элемента
+//Zakharow Timofey 123-mm
 void add_object() {
-    // Увеличение размера массива
+
     objects = (Object*)realloc(objects, (objectCount + 1) * sizeof(Object));
-    // Ввод данных с клавиатуры
-    printf("Введите текст: ");
+    printf("Imput name of object: ");
     scanf("%s", objects[objectCount].text);
-    printf("Введите вещественное число: ");
+    printf("Imput double num: ");
     scanf("%f", &objects[objectCount].floatValue);
-    printf("Введите целое число: ");
+    printf("Imput int num: ");
     scanf("%d", &objects[objectCount].intValue);
     objectCount++;
     printf("%d\n", objectCount);
 }
-// Функция для удаления элемента по номеру
 void delete_object(int index) {
     if (index < 0 || index >= objectCount) {
-        printf("Неверный номер\n");
+        printf("Number imput error\n");
         return;
     }
-    // Сдвиг всех элементов после удаляемого
     for (int i = index; i < objectCount - 1; i++) {
         objects[i] = objects[i + 1];
     }
-    // Уменьшение размера массива
     objects = (Object*)realloc(objects, (objectCount - 1) * sizeof(Object));
     objectCount--;
 }
