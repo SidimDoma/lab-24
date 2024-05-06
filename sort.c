8fd88879f350be168e1289e3a593275d8c92f1da
SidimDoma <167631201+SidimDoma@users.noreply.github.com>
2024-05-06 17:02:54 +0300

    sort.c

--- sort.c
+++ sort.c
@@ -2,32 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include "base.h"
-// Функция для сравнения двух объектов по текстовому полю
 int compareText(const void *a, const void *b) {
     Object *obj1 = (Object *)a;
     Object *obj2 = (Object *)b;
     return strcmp(obj1->text, obj2->text);
 }
 
-// Функция для сравнения двух объектов по вещественному полю
 int compareFloat(const void *a, const void *b) {
     Object *obj1 = (Object *)a;
     Object *obj2 = (Object *)b;
     return (obj1->floatValue > obj2->floatValue) - (obj1->floatValue < obj2->floatValue);
 }
 
-// Функция для сравнения двух объектов по целочисленному полю
 int compareInt(const void *a, const void *b) {
     Object *obj1 = (Object *)a;
     Object *obj2 = (Object *)b;
     return obj1->intValue - obj2->intValue;
 }
 
-// Функция для обобщенной сортировки по выбранному полю и направлению
 void sortObjectsByField(int field, int direction, int (*compareFunction)(const void *, const void *)) {
     qsort(objects, objectCount, sizeof(Object), compareFunction);
     if (direction == 2) {
-        // Если направление сортировки - по убыванию, переворачиваем массив
         for (int i = 0; i < objectCount / 2; i++) {
             Object temp = objects[i];
             objects[i] = objects[objectCount - i - 1];
