7621e528852e26f27962170e9f5fa2f5b342ffdd
SidimDoma <167631201+SidimDoma@users.noreply.github.com>
2024-05-06 17:03:14 +0300

    sort.h

--- sort.h
+++ sort.h
@@ -1,16 +1,12 @@
 #ifndef SORT_H_INCLUDED
 #define SORT_H_INCLUDED
 
-// Функция для сравнения двух объектов по текстовому полю
 int compareText(const void *a, const void *b);
 
-// Функция для сравнения двух объектов по вещественному полю
 int compareFloat(const void *a, const void *b);
 
-// Функция для сравнения двух объектов по целочисленному полю
 int compareInt(const void *a, const void *b);
 
-// Функция для обобщенной сортировки по выбранному полю и направлению
 void sortObjectsByField(int field, int direction, int (*compareFunction)(const void *, const void *));
 
 #endif // SORT_H_INCLUDED
